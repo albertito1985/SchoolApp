3d454f227159acda4b4c0361bdd107e0f29c77ca
albertito1985 <anulo@live.se>
2021-12-28 15:09:31 +0100

    Add files via upload

---
+++ cors-server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(cors({origin: "http://localhost:3000"}));
+app.use(express.json());
+
+app.get('/dataRequest/:lat/:long/:page', (req, res) => {
+    res.redirect(`https://api.skolverket.se/planned-educations/school-units?coordinateSystemType=0160&distance=5&latitude=${req.params.lat}&longitude=${req.params.long}&page=${req.params.page}&size=20&typeOfSchooling=gr`);
+})
+
+app.get('/schoolRequest/:id', (req, res) => {
+    res.redirect(`https://api.skolverket.se/planned-educations/school-units/${req.params.id}`);
+})
+
+app.get('/geoCoding/:adress', (req, res) => {
+    res.redirect(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.adress}&key=AIzaSyDioBnws44nU0R32P7ruqT1lhk9rwo7LQc`);
+})
+
+app.get('/StatisticsRequest/:id', (req, res) => {
+    if(req.params.id==="generall"){
+        res.redirect(`https://api.skolverket.se/planned-educations/statistics/national-values/gr`);
+    }else{
+        res.redirect(`https://api.skolverket.se/planned-educations/school-units/${req.params.id}/statistics/gr`);
+    }
+})
+
+
+
+
+app.listen(8080, () => {
+    console.log('listening on port 8080')
+})
